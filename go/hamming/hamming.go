8a8cf4fef8a4e636eabe394d82dc47de82868050
Marcelo Monaco <marcelo.monaco@gmail.com>
2016-05-04 19:13:52 -0300

    hamming

---
+++ go/hamming/hamming.go
@@ -0,0 +1,25 @@
+package hamming
+
+import (
+	"errors"
+)
+
+// testVersion should match the targetTestVersion in the test file.
+const testVersion = 4
+
+// Distance calculates the Hamming difference between two DNA strands
+func Distance(a, b string) (distance int, err error) {
+
+	if len(a) != len(b) {
+		return distance, errors.New("Wrong length")
+	}
+
+	for i := 0; i<len(a) && i<len(b); i++ {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+
+	return
+
+}
